66684cb14fed8b86064d44660551c2978c19e195
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2024-11-10 01:49:00 +0900

    feat : 주문을 입력받는 기능 추가
    주문멘트를 출력하고 readLine으로 입력받게끔 설정

---
+++ src/main/java/store/domain/InputDTO.java
@@ -0,0 +1,12 @@
+package store.domain;
+
+public class InputDTO {
+    private String input;
+
+    public InputDTO(String input) {
+        this.input = input;
+    }
+    public String getInput() {
+        return input;
+    }
+}
--- src/main/java/store/view/ConvenienceInputView.java
+++ src/main/java/store/view/ConvenienceInputView.java
@@ -1,4 +1,14 @@
 package store.view;
 
+import store.domain.InputDTO;
+import store.domain.OrderDTO;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class ConvenienceInputView {
+    public InputDTO inputProductAndQuantity(){
+        System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
+        return new InputDTO(readLine());
+    }
+
 }
