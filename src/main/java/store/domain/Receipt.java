bc1b59dee4a43ea66e183e45bbb080a9eef5f249
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2024-11-11 02:19:48 +0900

    feat : 영수증을 출력하는 기능 추가
    사용자에게 영수증을 출력하여주는 기능을 추가함

---
+++ src/main/java/store/domain/Receipt.java
@@ -0,0 +1,85 @@
+package store.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Receipt {
+    private final List<String> purchasedItems;
+    private final List<String> freeItems;
+    private int totalPurchaseAmount;
+    private int promotionDiscounts;
+    private int membershipDiscount;
+    private int finalAmount;
+    private int totalQuantity;
+    private int promotionAmount;
+
+    public Receipt() {
+        this.purchasedItems = new ArrayList<>();
+        this.freeItems = new ArrayList<>();
+        this.promotionDiscounts = 0;
+        this.totalPurchaseAmount = 0;
+        this.membershipDiscount = 0;
+        this.finalAmount = 0;
+        this.totalQuantity = 0;
+        this.promotionAmount = 0;
+    }
+
+    public int getFinalAmount() {
+        return finalAmount;
+    }
+
+    public int getMembershipDiscount() {
+        return membershipDiscount;
+    }
+
+    public int getTotalQuantity() {
+        return totalQuantity;
+    }
+
+    public int getTotalPurchaseAmount() {
+        return totalPurchaseAmount;
+    }
+
+    public int getPromotionAmount() {
+        return promotionAmount;
+    }
+
+    public void addPromotionAmount(int amount) {
+        this.promotionAmount += amount;
+    }
+
+    public void addPurchasedItem(String productName, int quantity, int price) {
+        purchasedItems.add(String.format("%-20s%-8d%-8s", productName, quantity, String.format("%,d", quantity * price)));
+        totalPurchaseAmount += quantity * price;
+        totalQuantity += quantity;
+    }
+
+    public void addFreeItem(String productName, int quantity) {
+        freeItems.add(String.format("%-20s%-9d", productName, quantity));
+    }
+
+    public void addPromotionDiscount(int discount) {
+        promotionDiscounts += discount;
+    }
+
+    public void applyMembershipDiscount(int discount) {
+        this.membershipDiscount = Math.min(discount, 8000); // 최대 8,000원 할인
+    }
+
+    public void calculateFinalAmount() {
+        finalAmount = totalPurchaseAmount - promotionDiscounts - membershipDiscount;
+    }
+
+    public int getTotalPromotionDiscount() {
+        return promotionDiscounts;
+    }
+
+    public void printPurchasedItems() {
+        purchasedItems.forEach(System.out::println);
+    }
+
+    public void printFreeItems() {
+        freeItems.forEach(System.out::println);
+    }
+
+}
--- src/main/java/store/view/ConvenienceOutputView.java
+++ src/main/java/store/view/ConvenienceOutputView.java
@@ -1,30 +1,51 @@
 package store.view;
 
 import store.domain.Product;
+import store.domain.Receipt;
 import store.domain.dto.ProductDTO;
 
 import java.util.List;
 
-//파일 입출력으로 가져와서 출력시켜주는 부분 필요함
 public class ConvenienceOutputView {
-    public void printWelcomeMessage(){
+    public void printWelcomeMessage() {
         System.out.println("안녕하세요. W편의점입니다.\n" + "현재 보유하고 있는 상품입니다.\n");
     }
 
-    public void printNowProducts(ProductDTO productDTO){
+    public void printNowProducts(ProductDTO productDTO) {
         List<Product> productList = productDTO.getProducts();
-        for(Product product : productList){
+        for (Product product : productList) {
             System.out.println(product.getFormattedOutput());
         }
     }
 
-    public void printPromotionOk(Product product,int promotionQuantity){
-        System.out.println("현재 "+ product.getProductName()+"은(는) "+promotionQuantity+"개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
+    public void printPromotionOk(Product product, int promotionQuantity) {
+        System.out.println("\n현재 " + product.getProductName() + "은(는) " + promotionQuantity + "개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
     }
 
-    public void printNoPromotion(Product product,int promotionQuantity){
-        System.out.println("현재 "+product.getProductName()+" "+promotionQuantity+"개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)");
+    public void printNoPromotion(Product product, int promotionQuantity) {
+        System.out.println("\n현재 " + product.getProductName() + " " + promotionQuantity + "개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)");
     }
 
+    public void printReceipt(Receipt receipt) {
+        System.out.println();
+        System.out.println("==============W 편의점================");
+        printItems(receipt);
+        System.out.println("=============증      정===============");
+        receipt.printFreeItems();
+        System.out.println("====================================");
+        printMoney(receipt);
+    }
+
+    private void printItems(Receipt receipt) {
+        System.out.printf("%-19s%-9s%-8s\n", "상품명", "수량", "금액");
+        receipt.printPurchasedItems();
+    }
+
+    private void printMoney(Receipt receipt) {
+        System.out.printf("%-19s%-9d%-8s\n", "총구매액", receipt.getTotalQuantity(), String.format("%,d", receipt.getTotalPurchaseAmount()));
+        System.out.printf("%-28s%-8s\n", "행사할인", String.format("-%,d", receipt.getTotalPromotionDiscount()));
+        System.out.printf("%-27s%-8s\n", "멤버십할인", String.format("-%,d", receipt.getMembershipDiscount()));
+        System.out.printf("%-29s%-8s\n", "내실돈", String.format("%,d", receipt.getFinalAmount()));
+    }
 
 }
