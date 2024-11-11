fc80a46ff271c00e8cd754fd506e9a72b6f96352
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2024-11-11 15:52:06 +0900

    style : 출력 형식 변경
    한 줄을 띄우고 편의점 안내 메세지가 나오게끔 수정함

--- src/main/java/store/view/ConvenienceOutputView.java
+++ src/main/java/store/view/ConvenienceOutputView.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class ConvenienceOutputView {
     public void printWelcomeMessage() {
-        System.out.println("안녕하세요. W편의점입니다.\n" + "현재 보유하고 있는 상품입니다.\n");
+        System.out.println("\n안녕하세요. W편의점입니다.\n" + "현재 보유하고 있는 상품입니다.\n");
     }
 
     public void printNowProducts(ProductDTO productDTO) {
